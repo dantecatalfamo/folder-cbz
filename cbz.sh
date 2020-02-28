3ae88117bd49677d187b2b1685caf3d63cae3e5b
Dante Catalfamo <dante.catalfamo@gmail.com>
2020-02-28 15:29:37 -0500

    No error if output exists, skip chapter if exists

--- cbz.sh
+++ cbz.sh
@@ -6,22 +6,23 @@ series=$(basename $path)
 echo "Series: $series"
 output=$(realpath "$path/../$series cbz")
 echo "Output: $output"
-mkdir "$output"
-
-echo "Chapters: $chapters"
+mkdir -p "$output"
 
 cd "$path"
-echo "In $path"
 
 chapters=$(ls)
-echo "Chapters: $chapters"
 
 IFS=$'\n'
 for chapter in $chapters; do
-    echo "Processing $chapter"
     cd "$chapter"
     chapternum=$(echo $chapter | cut -d ' ' -f 1)
+    echo "Processing $chapternum"
     chapterout="$output/$chapternum.cbz"
+    if [ -e $chapterout ]; then
+        echo "Chapter $chapternum.cbz already exists, skipping"
+        cd ..
+        continue
+    fi
     echo "Chapter out: $chapterout"
     zip -r "$chapterout" .
     cd ..
